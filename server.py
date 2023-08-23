2092e0bd5c6ad509bed70ed07e38f4a9bcd053cd
vishnudeep14 <vishnsteve4@gmail.com>
2023-08-24 02:29:49 +0530

    server changed again

--- server.py
+++ server.py
@@ -1,33 +1,26 @@
-from flask import Flask, request, jsonify
-
+from flask import Flask,request, jsonify
+from dataclasses import dataclass
 app = Flask(__name__)
+
+@dataclass
+class Result:
+    result : int
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return "Hello world!",200
+    return 'hello world!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    data = request.json
-    print(data)
-    if 'first' in data and 'second' in data:
-        first = data['first']
-        second = data['second']
-        result = first + second
-        return jsonify({'result-of-the-summation': result}),200
-    else:
-        return jsonify({'error': 'Invalid data'}), 400
+    data = request.get_json()
+    d = Result(data["first"] + data["second"])
+    return jsonify(d)
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    data = request.json
-    if 'first' in data and 'second' in data:
-        num1 = data['first']
-        num2 = data['second']
-        result = num1 - num2
-        return jsonify({'result-of-the-subtraction': result}),200
-    else:
-        return jsonify({'error': 'Invalid data'}), 400
+    data = request.get_json()
+    d = Result(data["first"] - data["second"])
+    return jsonify(d)
 
 if __name__ == '__main__':
-    app.run(port=8080, host='0.0.0.0')
+    app.run(port=8080,host='0.0.0.0')
