e462d933d50140fb678160ef69ea28856d5e2aec
vishnudeep14 <vishnsteve4@gmail.com>
2023-08-24 01:31:37 +0530

    Description of your changes

--- server.py
+++ server.py
@@ -1,19 +1,33 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify
+import requests
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return "Hello Word",200
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
-
-@app.route("/calculator/subtract", methods=['POST'])
+    data=request.json
+    if "first" in data and 'second' in data:
+        first=data['first']
+        second=data['second']
+        res=first+second
+        return jsonify({'result-of-the-summation:',res}),200
+    else:
+        return jsonify({'error': 'Invalid data'}), 400
+   
+@app.route("/calculator/sub", methods=['POST'])
 def subtract():
-    return ''
+    data=request.json
+    if "first" in data and 'second' in data:
+        first=data['first']
+        second=data['second']
+        res=first-second
+        return jsonify({'result-of-the-subtraction',res}),200
+    else:
+        return jsonify({'error': 'Invalid data'}), 400
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
